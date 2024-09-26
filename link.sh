04406592070fecead1e967c0693055cb4de381b3
yuhi <yuhi.narita26@gmail.com>
2024-09-26 12:16:46 +0800

    auto install plugins

--- link.sh
+++ link.sh
@@ -5,5 +5,6 @@ for file in .*; do
   if [[ "$file" == "." || "$file" == ".." || "$file" == ".git" ]]; then
     continue
   fi
-  ln -s ${PWD}/${file} ~/
+  ln -sf ${PWD}/${file} ~/
+  vim -es -u vimrc -i NONE -c "PlugInstall" -c "qa"
 done
